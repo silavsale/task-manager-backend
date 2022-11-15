ecd377f4e03e120ac08b85207b52217bf43c1d66
Sergiy Rudenko <silavsale@gmail.com>
2022-11-14 23:45:20 -0500

    started to add routes

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,11 @@ import express from "express";
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("GET endpoint");
+app.get("/hello", (req, res) => {
+  res.send("Task manager App");
 });
+
+
 
 app.listen(port, () => {
   return console.log(`Express is listening at http://localhost:${port}`);
---
+++ src/routes/tasks.ts
@@ -0,0 +1,13 @@
+import express from "express";
+const router = express.Router();
+
+//need to create controller for get all tasks
+// router.route("/", getAllTask);
+
+// app.get('/api/v1/tasks') - get all tasks
+// app.get('/api/v1/tasks') - get task by ID
+// app.post('/api/v1/tasks') - create new task
+// app.delete('/api/v1/tasks') - delete task by ID
+// app.patch('/api/v1/tasks') - update task by ID
+
+module.exports = router;
